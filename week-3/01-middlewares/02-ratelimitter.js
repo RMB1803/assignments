445b10092459be89587fcab36403cb47faecfdd3
Ram <rammohanofficial03@gmail.com>
2024-07-24 14:11:24 +0530

    Middlewares and JWT assignments done

--- week-3/01-middlewares/02-ratelimitter.js
+++ week-3/01-middlewares/02-ratelimitter.js
@@ -16,6 +16,23 @@ setInterval(() => {
     numberOfRequestsForUser = {};
 }, 1000)
 
+app.use(function(req, res, next) {
+  const userID = req.headers["user-id"]
+
+  if(numberOfRequestsForUser[userID]) {
+    numberOfRequestsForUser[userID]++
+
+    if(numberOfRequestsForUser[userID] > 5) {
+      res.status(404).send("No entry")
+    } else {
+      next()
+    }
+  } else {
+    numberOfRequestsForUser[userID] = 1;
+    next()
+  }
+})
+
 app.get('/user', function(req, res) {
   res.status(200).json({ name: 'john' });
 });
